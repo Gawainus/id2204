6194963196d00353f02b6273033acbcb1917c1f5
Yumen Tsao <gawainus97@gmail.com>
2016-04-27 17:31:45 +0200

    redid constructor

--- A3/square.cpp
+++ A3/square.cpp
@@ -34,13 +34,16 @@ using namespace Gecode;
 
 class Square : public Script {
 protected:
-  IntVar s;
+  unsigned int n; // number of boxes we are trying to fit in
+  IntVar s; // side of the enclosing box
   IntVarArray x;
   IntVarArray y;
+
 private:
   static int size(int n, int i){
     return n-i;
   }
+
 public:
   // Branching variants
   enum {
@@ -50,17 +53,14 @@ public:
     BRANCH_TOP_FIRST    ///< Try to place squares from top to bottom
   };
   
-  Square(const SizeOptions& opt) :
-    Script(opt),
-    s(*this, sqrt(opt.size()*(opt.size()+1)*(2*opt.size()+1)/6), opt.size()*sqrt(opt.size()+1)),
-    // ignore 1*1 square
-    x(*this, opt.size()-1, 0, opt.size()*sqrt(opt.size()+1)-1),
-    y(*this, opt.size()-1, 0, opt.size()*sqrt(opt.size()+1)-1) {
+  Square(const SizeOptions& opt): Script(opt) {
+    n = opt.size();
+    s = IntVar(*this, sqrt(n*(n+1)*(2*n+1)/6), n*sqrt(n+1));
+    x = IntVarArray(*this, n-1, 0, n*sqrt(n+1)-1);
+    y = IntVarArray(*this, n-1, 0, n*sqrt(n+1)-1);
 
-    int n = opt.size();
-    
     // max coordinates
-    for (int i=0; i<n-1; i++){
+    for (int i=0; i<n-1; i++) {
       rel(*this, x[i]<=s-size(n,i));
       rel(*this, y[i]<=s-size(n,i));
     }
@@ -68,58 +68,58 @@ public:
     rel(*this, x[0]<=(s-n)/2);
     rel(*this, y[0]<=x[0]);
 
-    for (int i=0; i<n-1; i++){
+    for (int i=0; i<n-1; i++) {
       if (size(n,i)==45){
-	rel(*this, x[i]!=10);
-	rel(*this, y[i]!=10);
+	      rel(*this, x[i]!=10);
+	      rel(*this, y[i]!=10);
       }
-      else if (size(n,i)>=34){
-	rel(*this, x[i]!=9);
-	rel(*this, y[i]!=9);
+      else if (size(n,i)>=34) {
+	      rel(*this, x[i]!=9);
+	      rel(*this, y[i]!=9);
       }
       else if (size(n,i)>=30){
-	rel(*this, x[i]!=8);
-	rel(*this, y[i]!=8);
+        rel(*this, x[i]!=8);
+        rel(*this, y[i]!=8);
       }
       else if (size(n,i)>=22){
-	rel(*this, x[i]!=7);
-	rel(*this, y[i]!=7);
+        rel(*this, x[i]!=7);
+        rel(*this, y[i]!=7);
       }
       else if (size(n,i)>=18){
-	rel(*this, x[i]!=6);
-	rel(*this, y[i]!=6);
+        rel(*this, x[i]!=6);
+        rel(*this, y[i]!=6);
       }
       else if (size(n,i)>=12){
-	rel(*this, x[i]!=5);
-	rel(*this, y[i]!=5);
+        rel(*this, x[i]!=5);
+        rel(*this, y[i]!=5);
       }
       else if (size(n,i)>=9){
-	rel(*this, x[i]!=4);
-	rel(*this, y[i]!=4);
+	      rel(*this, x[i]!=4);
+        rel(*this, y[i]!=4);
       }
       else if (size(n,i)>=5){
-	rel(*this, x[i]!=3);
-	rel(*this, y[i]!=3);
+        rel(*this, x[i]!=3);
+        rel(*this, y[i]!=3);
       }
       else if (size(n,i)>=2){
-	rel(*this, x[i]!=2);
-	rel(*this, y[i]!=2);
+        rel(*this, x[i]!=2);
+        rel(*this, y[i]!=2);
       }
       if (size(n,i)==3){
-	rel(*this, x[i]!=3);
-	rel(*this, y[i]!=3);
+        rel(*this, x[i]!=3);
+	      rel(*this, y[i]!=3);
       }
       if (size(n,i)==2){
-	rel(*this, x[i]!=1);
-	rel(*this, y[i]!=1);
+	      rel(*this, x[i]!=1);
+	      rel(*this, y[i]!=1);
       }
     }
 
     // no overlap - disjoint
     for (int i=0; i<n-1; i++){
       for (int j=i+1; j<n-1; j++){
-	rel(*this, x[j]>=x[i]+size(n,i) || x[i]>=x[j]+size(n,j) ||
-	    y[i]>=y[j]+size(n,j) || y[j]>=y[i]+size(n,i));
+        rel(*this, x[j]>=x[i]+size(n,i) || x[i]>=x[j]+size(n,j) ||
+            y[i]>=y[j]+size(n,j) || y[j]>=y[i]+size(n,i));
       }
     }
 
@@ -127,18 +127,18 @@ public:
     for (int k=0; k<n*sqrt(n+1); k++){
       BoolVarArray bc(*this, n-1, 0, 1);
       for (int i=0; i<n-1; i++){
-	//bc[i] = expr(*this, (x[i] <= k) && (x[i]+size(n,i)>k));
-	dom(*this, x[i], k-size(n,i)+1, k, bc[i]);
+	      //bc[i] = expr(*this, (x[i] <= k) && (x[i]+size(n,i)>k));
+	      dom(*this, x[i], k-size(n,i)+1, k, bc[i]);
       }
       rel(*this, sum(IntArgs::create(n-1,n,-1), bc) <= s);
     }
 
     // no overlap - cumulative row
-    for (int k=0; k<n*sqrt(n+1); k++){
+    for (int k=0; k<n*sqrt(n+1); k++) {
       BoolVarArray br(*this, n-1, 0, 1);
-      for (int i=0; i<n-1; i++){
-	//br[i] = expr(*this, (y[i] <= k) && (y[i]+size(n,i)>k));
-	dom(*this, y[i], k-size(n,i)+1, k, br[i]);
+      for (int i=0; i<n-1; i++) {
+        //br[i] = expr(*this, (y[i] <= k) && (y[i]+size(n,i)>k));
+        dom(*this, y[i], k-size(n,i)+1, k, br[i]);
       }
       rel(*this, sum(IntArgs::create(n-1,n,-1), br) <= s);
     }
@@ -148,15 +148,18 @@ public:
     if (opt.branching() == BRANCH_X_FIRST) {
       branch(*this, x, INT_VAR_SIZE_MIN(), INT_VAL_MIN());
       branch(*this, y, INT_VAR_SIZE_MIN(), INT_VAL_MIN());
-    } else if (opt.branching() == BRANCH_BIG_FIRST) {
+    }
+    else if (opt.branching() == BRANCH_BIG_FIRST) {
       for (int i = 0; i<n-1; i++){
-	branch(*this, x[i], INT_VAL_MIN());
-	branch(*this, y[i], INT_VAL_MIN());
+	      branch(*this, x[i], INT_VAL_MIN());
+	      branch(*this, y[i], INT_VAL_MIN());
       }
-    } else if (opt.branching() == BRANCH_LEFT_FIRST) {
+    }
+    else if (opt.branching() == BRANCH_LEFT_FIRST) {
       branch(*this, x, INT_VAR_MIN_MIN(), INT_VAL_MIN());
       branch(*this, y, INT_VAR_SIZE_MIN(), INT_VAL_MIN());
-    } else if (opt.branching() == BRANCH_TOP_FIRST) {
+    }
+    else if (opt.branching() == BRANCH_TOP_FIRST) {
       branch(*this, y, INT_VAR_MAX_MAX(), INT_VAL_MAX());
       branch(*this, x, INT_VAR_SIZE_MIN(), INT_VAL_MAX());
     }
@@ -167,9 +170,11 @@ public:
     x.update(*this, share, square.x);
     y.update(*this, share, square.y);
   }
+
   virtual Space* copy(bool share) {
     return new Square(share,*this);
   }
+
   virtual void print(std::ostream& os) const {
     os << "s = " << s << std::endl << std::endl;
     for (int i = 0; i<x.size(); i++){
